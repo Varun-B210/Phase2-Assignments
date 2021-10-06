a0ecbeec6dab26256b68acd0561cc9e78611e7f5
Varun-B210 <89086127+Varun-B210@users.noreply.github.com>
2021-10-06 20:27:34 +0530

    User Login assignment submitted

---
+++ UserLogin/src/main/java/com/LoginFilter.java
@@ -0,0 +1,57 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+public class LoginFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		PrintWriter out=response.getWriter();
+		String username =request.getParameter("username");
+		String password =request.getParameter("password");
+		 System.out.println("Username in Filter is :- " + username);
+		 if(username.equalsIgnoreCase("varun")&&password.equals("123")) {
+		chain.doFilter(request, response);
+		 }
+		 else {
+		out.print("Invalid Input");
+	}
+		 
+			 
+		 }
+	
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
